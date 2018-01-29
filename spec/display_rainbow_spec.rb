27693d6de1a5b5f9bd25643db5b0006ce47fab81
Natalie Tateyama <natalie.tateyama@gmail.com>
2018-01-29 15:10:49 +0000

    automatically backed up by learn

--- spec/display_rainbow_spec.rb
+++ spec/display_rainbow_spec.rb
@@ -9,7 +9,7 @@ describe '#display_rainbow' do
   end
 
   it 'prints out the colors of the rainbow correctly when passed in in order' do
-
+    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
     expect(colors).to receive(:[]).with(0).at_least(:once).and_return("red")
     expect(colors).to receive(:[]).with(1).at_least(:once).and_return("orange")
     expect(colors).to receive(:[]).with(2).at_least(:once).and_return("yellow")
